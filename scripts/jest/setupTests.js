ed4536721f7cbce1359f090ade655e2f065d171e
Martin Zlámal <mrtnzlml@gmail.com>
2021-06-19 14:44:31 -0500

    Publish new package `@adeira/jest-disallow-console`
    This is a followup after https://github.com/facebook/jest/issues/11456. Basically, our previous solution stopped working reliably and after a discussion with Jest team I realized that it's an ugly hack and we are lucky it worked for so long.
    
    This change introduces a new `@adeira/jest-disallow-console` package which helps with testing console warnings.
    
    The code is heavily inspired by `warnings.js` from `facebook/relay` (see: https://github.com/facebook/relay/blob/57dde664137a221a4633d1c02592b2a4e17d3feb/packages/relay-test-utils-internal/warnings.js) but it's modified to work with `global.console` instead of `warning` module.

--- scripts/jest/setupTests.js
+++ scripts/jest/setupTests.js
@@ -1,10 +1,6 @@
 // @flow strict
 
-/* eslint-disable no-console */
-
-import util from 'util';
 import os from 'os';
-import chalk from 'chalk';
 
 /**
  * @see: https://facebook.github.io/jest/docs/expect.html#expectextendmatchers
@@ -30,67 +26,4 @@ expect.extend({
       message: () => `expected ${received} to be instance of Error, ${typeof received} given`,
     };
   },
-});
-
-type MaybeSpy =
-  | {
-      +calls?: {
-        count: mixed,
-        ...
-      },
-      ...
-    }
-  | { ... };
-
-const isSpy = (spy: MaybeSpy): boolean %checks => {
-  return spy.calls != null && typeof spy.calls.count === 'function';
-};
-
-// TODO: .toWarnDev() ?
-// Inspired by React itself: https://github.com/facebook/react/blob/7841d0695ae4bde9848cf8953baf34d312d0cced/scripts/jest/setupTests.js
-['error', 'warn', 'log', 'info', 'groupCollapsed'].forEach((methodName) => {
-  const unexpectedConsoleCallStacks = [];
-  const newMethod = function (format, ...args) {
-    const stack = new Error().stack;
-    unexpectedConsoleCallStacks.push([
-      stack.substr(stack.indexOf('\n') + 1),
-      util.format(format, ...args),
-    ]);
-  };
-
-  // $FlowExpectedError[cannot-write]: these properties are not normally writable but it's expected in this case
-  console[methodName] = newMethod;
-
-  // TODO: which back to beforeEach, see https://github.com/facebook/jest/issues/11456
-  global.beforeAll(() => {
-    unexpectedConsoleCallStacks.length = 0;
-  });
-
-  // TODO: which back to afterEach, see https://github.com/facebook/jest/issues/11456
-  global.afterAll(() => {
-    if (console[methodName] !== newMethod && !isSpy(console[methodName])) {
-      throw new Error(
-        `Test did not tear down console.${methodName} mock properly. Did you call spy.mockRestore() or jest.restoreAllMocks()?`,
-      );
-    }
-
-    if (unexpectedConsoleCallStacks.length > 0) {
-      const messages = unexpectedConsoleCallStacks.map(
-        ([stack, message]) =>
-          `${chalk.red(message)}\n` +
-          `${stack
-            .split('\n')
-            .map((line) => chalk.gray(line))
-            .join('\n')}`,
-      );
-
-      const message =
-        `Expected test not to call ${chalk.bold(`console.${methodName}()`)}.\n\n` +
-        `If the console output is expected, test for it explicitly by mocking it out using ${chalk.bold(
-          'jest.spyOn',
-        )}(console, '${methodName}').mockImplementation(...) and test that the output occurs.`;
-
-      throw new Error(`${message}\n\n${messages.join('\n\n')}`);
-    }
-  });
 });
